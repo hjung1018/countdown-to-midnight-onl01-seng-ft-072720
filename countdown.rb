1ae04aaf6312bc2fc1e4808f226b9c5db4293f11
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-07-29 12:58:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def countdown(seconds_to_midnight)
   while seconds_to_midnight > 0
     puts "#{seconds_to_midnight} SECOND(S)!"
@@ -13,4 +14,15 @@ def countdown_with_sleep(seconds_to_midnight)
     seconds_to_midnight -= 1
   end
   "HAPPY NEW YEAR!"
+=======
+#write your code here
+number = 10
+def countdown
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  if number ==  0 
+    return "HAPPY NEW YEAR!"
+end
+>>>>>>> ca90a1457feae93654ac299e8396399f98c7035b
 end
